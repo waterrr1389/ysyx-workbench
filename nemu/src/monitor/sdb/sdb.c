b48d757c176d80b4b71721e252e59d189779c80b
tracer-ysyx <tracer@ysyx.org>
2025-03-08 21:17:31 +0800

    > compile NEMU ysyx_22040000 谭智健 Linux ysyx-waterrr 6.8.0-52-generic #53~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Jan 15 19:18:46 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  21:17:31 up 23:27,  3 users,  load average: 0.65, 0.57, 0.50

--- nemu/src/monitor/sdb/sdb.c
+++ nemu/src/monitor/sdb/sdb.c
@@ -85,9 +85,8 @@ static int cmd_x(char* args) {
   if (strlen(args) < 2) {
     return 1; 
   } else {
-    printf("%s", args);
+    printf("%s\n", args);
     int n = args[0] - '0';
-    printf("%d", n);
     int val = cmd_expr(args+1);
     int* addr = (int*)(uint64_t)(uint32_t)val;
     for (int i = 0; i < n; i++) {
