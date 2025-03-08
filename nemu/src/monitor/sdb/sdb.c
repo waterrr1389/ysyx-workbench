737528df0f4375da7c55cc9c4f4afe24bd2c232e
tracer-ysyx <tracer@ysyx.org>
2025-03-08 17:31:11 +0800

    > compile NEMU ysyx_22040000 谭智健 Linux ysyx-waterrr 6.8.0-52-generic #53~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Jan 15 19:18:46 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  17:31:11 up 19:41,  3 users,  load average: 0.41, 0.50, 0.44

--- nemu/src/monitor/sdb/sdb.c
+++ nemu/src/monitor/sdb/sdb.c
@@ -47,12 +47,38 @@ static int cmd_c(char *args) {
   return 0;
 }
 
-
 static int cmd_q(char *args) {
   return -1;
 }
 
 static int cmd_help(char *args);
+
+static int cmd_si(char* args) {
+  int num_inst = args[0]; 
+  cpu_exec(num_inst);
+  return 0;
+}
+
+static int cmd_info(char* args) {
+  isa_reg_display();
+  return 0;
+}
+
+static int cmd_x(char* args) {
+  return 0;
+}
+
+static int cmd_expr(char* args) {
+  return 0;
+}
+
+static int cmd_w(char* args) {
+  return 0;
+}
+
+static int cmd_d(char* args) {
+  return 0;
+}
 
 static struct {
   const char *name;
@@ -62,7 +88,12 @@ static struct {
   { "help", "Display information about all supported commands", cmd_help },
   { "c", "Continue the execution of the program", cmd_c },
   { "q", "Exit NEMU", cmd_q },
-
+  { "si",   "Execute N instructions then pause (default N=1)", cmd_si },
+  { "info", "Print register status or watchpoint information (info r|w)", cmd_info },
+  { "x",    "Scan memory: x N EXPR - Display N*4 bytes from address EXPR", cmd_x },
+  { "p",    "Evaluate expression: p EXPR - Calculate value of EXPR", cmd_expr },
+  { "w",    "Set watchpoint: w EXPR - Pause when EXPR value changes", cmd_w },
+  { "d",    "Delete watchpoint: d N - Remove watchpoint number N", cmd_d },
   /* TODO: Add more commands */
 
 };
