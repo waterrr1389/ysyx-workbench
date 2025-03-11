744d6291c55d555c0583212c839bbf49db509d12
tracer-ysyx <tracer@ysyx.org>
2025-03-11 23:58:19 +0800

    > compile NEMU ysyx_22040000 谭智健 Linux ysyx-waterrr 6.8.0-52-generic #53~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Jan 15 19:18:46 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  23:58:19 up 44 min,  1 user,  load average: 0.24, 0.24, 0.34

--- nemu/src/nemu-main.c
+++ nemu/src/nemu-main.c
@@ -19,6 +19,7 @@ void init_monitor(int, char *[]);
 void am_init_monitor();
 void engine_start();
 int is_exit_status_bad();
+int test();
 
 int main(int argc, char *argv[]) {
   /* Initialize the monitor. */
@@ -30,6 +31,7 @@ int main(int argc, char *argv[]) {
 
   /* Start engine. */
   engine_start();
+  test();
 
   return is_exit_status_bad();
 }
--- nemu/tools/gen-expr/gen-expr.c
+++ nemu/tools/gen-expr/gen-expr.c
@@ -21,6 +21,7 @@
 #include <string.h>
 
 // this should be enough
+#define MAX_DEPTH 10
 static char buf[65536] = {};
 static char code_buf[65536 + 128] = {}; // a little larger than `buf`
 static char *code_format =
@@ -31,8 +32,53 @@ static char *code_format =
 "  return 0; "
 "}";
 
-static void gen_rand_expr() {
-  buf[0] = '\0';
+static uint32_t choose(uint32_t n) {
+  return ((uint32_t)rand()) % n;
+}
+
+static void gen_space() {
+  int num = choose(3);
+  if (num != 1) return;
+  for (int i = 0; i < num && strlen(buf) < 60000; i++) {
+    strcat(buf, " ");
+  }
+}
+
+static void gen_num() {
+  char num[16];
+  sprintf(num, "%u", (uint32_t)rand()%1000 + 1);
+  strcat(buf, num);
+  gen_space();
+}
+
+static void gen_rand_op() {
+  uint32_t n = choose(4); //0-3
+  char op[4] = {'+', '-', '*', '/'};
+  char op_ch = op[n];
+  char op_str[2] = {op_ch, '\0'};
+  strcat(buf, op_str);
+  //if (n == 3) gen_num();
+  gen_space();
+}
+
+static void gen(char c) {
+  char str[2] = {c, '\0'};
+  strcat(buf, str);
+  gen_space();
+}
+
+static void gen_rand_expr(uint32_t depth) {
+  if (depth <= 0 || strlen(buf) > 60000) {
+    gen_num();
+    return;
+  }
+  else {
+    switch (choose(3)) {
+      case 0: gen_num(); break;
+      case 1: gen('('); gen_rand_expr(depth-1); gen(')'); break;
+      default: gen_rand_expr(depth-1); gen_rand_op(); gen_rand_expr(depth-1); break;
+    }
+  }
 }
 
 int main(int argc, char *argv[]) {
@@ -44,7 +90,8 @@ int main(int argc, char *argv[]) {
   }
   int i;
   for (i = 0; i < loop; i ++) {
-    gen_rand_expr();
+    buf[0] = '\0';
+    gen_rand_expr(MAX_DEPTH);
 
     sprintf(code_buf, code_format, buf);
 
