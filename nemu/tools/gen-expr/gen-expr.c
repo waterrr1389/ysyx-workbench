78afe37ca7a224cc61c7705895cac8f95ec3df0a
tracer-ysyx <tracer@ysyx.org>
2025-03-28 21:54:54 +0800

    > compile NEMU ysyx_22040000 谭智健 Linux ysyx-waterrr 6.8.0-52-generic #53~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Jan 15 19:18:46 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  21:54:54 up  2:27,  3 users,  load average: 1.05, 0.81, 0.80

--- nemu/tools/gen-expr/gen-expr.c
+++ nemu/tools/gen-expr/gen-expr.c
@@ -21,7 +21,7 @@
 #include <string.h>
 
 // this should be enough
-#define MAX_DEPTH 10
+#define MAX_DEPTH 5
 static char buf[65536] = {};
 static char code_buf[65536 + 128] = {}; // a little larger than `buf`
 static char *code_format =
