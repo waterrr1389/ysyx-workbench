c4d0cd8369f234fd78ed9d2e8d8eddf3eeb74dbd
waterrr <riyant258316@gmail.com>
2025-05-09 19:37:38 +0800

    修复一处

--- nemu/src/cpu/cpu-exec.c
+++ nemu/src/cpu/cpu-exec.c
@@ -46,6 +46,7 @@ static void trace_and_difftest(Decode *_this, vaddr_t dnpc) {
   }
   IFDEF(CONFIG_DIFFTEST, difftest_step(_this->pc, dnpc));
 
+  check_watchpoints();
 }
 
 static void exec_once(Decode *s, vaddr_t pc) {
