ab41d37236b8528d580bc830eb35cd534eae0109
waterrr1389 <riyant258316@gamail.com>
2025-12-09 19:39:22 +0800

    增加对32位ELF文件的支持

--- nemu/src/monitor/sdb/ftrace.c
+++ nemu/src/monitor/sdb/ftrace.c
@@ -1,55 +1,62 @@
 #include "ftrace.h"
 
-static Elf64_Shdr symtab, strtab;
+static Elf_Shdr symtab, strtab;
 static FILE* fp;
 static char* strtab_buf;
-static Elf64_Sym* symbols;
+static Elf_Sym* symbols;
 
 // itrace init
 void readelf(const char* path) {
+    if (path == NULL) return;
+
     fp = fopen(path, "r");
-    Assert(fp == NULL, "%s\n", "ERROR: file does not exist"); 
+    Assert(fp != NULL, "%s\n", "ERROR: file does not exist"); 
     int number;
 
-    // Seek for the section header offset
-    number = fseek(fp, SH_OFF, SEEK_SET);
-    Assert( number != SH_OFF, "%s\n", "Failed to direct to section header");
-
-    Elf64_Ehdr Ehdr;
-    // Read the section header table offset
-    number = fread((void*)&Ehdr, sizeof(Elf64_Off), 1,fp);
-    Assert(number != sizeof(Elf64_Off), "%s\n", "Faield to read offset"); 
+    // Read the ELF executable header
+    Elf_Ehdr Ehdr;
+    number = fread((void*)&Ehdr, sizeof(Elf_Ehdr), 1, fp);
+    Assert(number == 1, "%s\n", "Faield to read offset"); 
     
-    // fseek to section header table
+    // Seek to section header table
     number = fseek(fp,Ehdr.e_shoff, SEEK_SET);
-    Assert(number != sizeof(Elf64_Shdr), "%s\n", "Failed to read section table entry");
+    Assert(number == 0, "%s\n", "Failed to direct to section table");
 
-    Elf64_Shdr tmp;
-    for (Elf64_Half count = Ehdr.e_shnum; count > 0; count--) {
-        // Read .symtab && .strtab
-        fread((void*)&tmp, sizeof(Elf64_Shdr), 1, fp);
-        if (tmp.sh_type == SHT_STRTAB) { 
-           strtab = tmp; 
-        } else if (tmp.sh_type == SHT_SYMTAB) {
-           symtab = tmp;
+    Elf_Shdr tmp;
+    for (int i = 0; i < Ehdr.e_shnum; i++) {
+        // 依次读取每一个 Section Header
+        number = fread((void*)&tmp, sizeof(Elf_Shdr), 1, fp);
+        Assert(number == 1, "%s\n", "Failed to read section header");
+
+        if (tmp.sh_type == SHT_SYMTAB) {
+            symtab = tmp;
+        } 
+        else if (tmp.sh_type == SHT_STRTAB) {
+            // 如果当前的索引 i 等于 Ehdr 中记录的 e_shstrndx
+            // 我们需要的是 .strtab (用来存函数变量名的)
+            if (i != Ehdr.e_shstrndx) {
+                strtab = tmp;
+            }
         }
     }
 
-    // Allocate space for strtab
+    // Allocate memory for string table
     strtab_buf = malloc(strtab.sh_size+1);
     fseek(fp, strtab.sh_offset, SEEK_SET); 
     fread(strtab_buf, strtab.sh_size, 1, fp);
 
-    // Alloc memory for all the symbols
-    uint32_t symtab_count = symtab.sh_size / sizeof(Elf64_Sym);
-    symbols = malloc(symtab_count * sizeof(Elf64_Sym));
+    // Alloccate memory for symbol table
+    uint32_t symtab_count = symtab.sh_size / sizeof(Elf_Sym);
+    symbols = malloc(symtab_count * sizeof(Elf_Sym));
     fseek(fp, symtab.sh_offset, SEEK_SET);
-    fread(symbols, sizeof(Elf64_Sym), symtab_count, fp);
+    fread(symbols, sizeof(Elf_Sym), symtab_count, fp);
 }
 
-// implement itrace
-void func_todo(word_t pc) {
-    
-
+// implement ftrace
+void ftrace(word_t pc) {
+    printf("Hello from ftrace\n\
+            Offset of symtab is 0x%x\n\
+            Offset of strtab is 0x%x\n", 
+            symtab.sh_offset,
+            strtab.sh_offset);
 }
-
--- nemu/src/monitor/sdb/ftrace.h
+++ nemu/src/monitor/sdb/ftrace.h
@@ -1,46 +1,66 @@
 #pragma once
 #include <common.h>
-#define SH_OFF 0x28
 #define SHT_SYMTAB	  2		/* Symbol table */
 #define SHT_STRTAB	  3		/* String table */
 
 /* Type for a 16-bit quantity.  */
+typedef uint16_t Elf32_Half;
 typedef uint16_t Elf64_Half;
 
 /* Types for signed and unsigned 32-bit quantities.  */
+typedef uint32_t Elf32_Word;
+typedef	int32_t  Elf32_Sword;
 typedef uint32_t Elf64_Word;
 typedef	int32_t  Elf64_Sword;
 
 /* Types for signed and unsigned 64-bit quantities.  */
+typedef uint64_t Elf32_Xword;
+typedef	int64_t  Elf32_Sxword;
 typedef uint64_t Elf64_Xword;
 typedef	int64_t  Elf64_Sxword;
 
 /* Type of addresses.  */
+typedef uint32_t Elf32_Addr;
 typedef uint64_t Elf64_Addr;
 
 /* Type of file offsets.  */
+typedef uint32_t Elf32_Off;
 typedef uint64_t Elf64_Off;
 
 /* Type for section indices, which are 16-bit quantities.  */
+typedef uint16_t Elf32_Section;
 typedef uint16_t Elf64_Section;
 
-typedef struct
-{
-  Elf64_Word	sh_name;		/* Section name (string tbl index) */
-  Elf64_Word	sh_type;		/* Section type */
-  Elf64_Xword	sh_flags;		/* Section flags */
-  Elf64_Addr	sh_addr;		/* Section virtual addr at execution */
-  Elf64_Off	    sh_offset;		/* Section file offset */
-  Elf64_Xword	sh_size;		/* Section size in bytes */
-  Elf64_Word	sh_link;		/* Link to another section */
-  Elf64_Word	sh_info;		/* Additional section information */
-  Elf64_Xword	sh_addralign;		/* Section alignment */
-  Elf64_Xword	sh_entsize;		/* Entry size if section holds table */
-} Elf64_Shdr;
+/* Type for version symbol information.  */
+typedef Elf32_Half Elf32_Versym;
+typedef Elf64_Half Elf64_Versym;
+
+
+/* The ELF file header.  This appears at the start of every ELF file.  */
+
+#define EI_NIDENT (16)
 
 typedef struct
 {
-  unsigned char	e_ident[16];	/* Magic number and other info */
+  unsigned char	e_ident[EI_NIDENT];	/* Magic number and other info */
+  Elf32_Half	e_type;			/* Object file type */
+  Elf32_Half	e_machine;		/* Architecture */
+  Elf32_Word	e_version;		/* Object file version */
+  Elf32_Addr	e_entry;		/* Entry point virtual address */
+  Elf32_Off	e_phoff;		/* Program header table file offset */
+  Elf32_Off	e_shoff;		/* Section header table file offset */
+  Elf32_Word	e_flags;		/* Processor-specific flags */
+  Elf32_Half	e_ehsize;		/* ELF header size in bytes */
+  Elf32_Half	e_phentsize;		/* Program header table entry size */
+  Elf32_Half	e_phnum;		/* Program header table entry count */
+  Elf32_Half	e_shentsize;		/* Section header table entry size */
+  Elf32_Half	e_shnum;		/* Section header table entry count */
+  Elf32_Half	e_shstrndx;		/* Section header string table index */
+} Elf32_Ehdr;
+
+typedef struct
+{
+  unsigned char	e_ident[EI_NIDENT];	/* Magic number and other info */
   Elf64_Half	e_type;			/* Object file type */
   Elf64_Half	e_machine;		/* Architecture */
   Elf64_Word	e_version;		/* Object file version */
@@ -56,7 +76,47 @@ typedef struct
   Elf64_Half	e_shstrndx;		/* Section header string table index */
 } Elf64_Ehdr;
 
-// Symbol table entry
+/* Section header.  */
+
+typedef struct
+{
+  Elf32_Word	sh_name;		/* Section name (string tbl index) */
+  Elf32_Word	sh_type;		/* Section type */
+  Elf32_Word	sh_flags;		/* Section flags */
+  Elf32_Addr	sh_addr;		/* Section virtual addr at execution */
+  Elf32_Off	sh_offset;		/* Section file offset */
+  Elf32_Word	sh_size;		/* Section size in bytes */
+  Elf32_Word	sh_link;		/* Link to another section */
+  Elf32_Word	sh_info;		/* Additional section information */
+  Elf32_Word	sh_addralign;		/* Section alignment */
+  Elf32_Word	sh_entsize;		/* Entry size if section holds table */
+} Elf32_Shdr;
+
+typedef struct
+{
+  Elf64_Word	sh_name;		/* Section name (string tbl index) */
+  Elf64_Word	sh_type;		/* Section type */
+  Elf64_Xword	sh_flags;		/* Section flags */
+  Elf64_Addr	sh_addr;		/* Section virtual addr at execution */
+  Elf64_Off	sh_offset;		/* Section file offset */
+  Elf64_Xword	sh_size;		/* Section size in bytes */
+  Elf64_Word	sh_link;		/* Link to another section */
+  Elf64_Word	sh_info;		/* Additional section information */
+  Elf64_Xword	sh_addralign;		/* Section alignment */
+  Elf64_Xword	sh_entsize;		/* Entry size if section holds table */
+} Elf64_Shdr;
+
+/* Symbol table entry.  */
+typedef struct
+{
+  Elf32_Word	st_name;		/* Symbol name (string tbl index) */
+  Elf32_Addr	st_value;		/* Symbol value */
+  Elf32_Word	st_size;		/* Symbol size */
+  unsigned char	st_info;		/* Symbol type and binding */
+  unsigned char	st_other;		/* Symbol visibility */
+  Elf32_Section	st_shndx;		/* Section index */
+} Elf32_Sym;
+
 typedef struct
 {
   Elf64_Word	st_name;		/* Symbol name (string tbl index) */
@@ -67,5 +127,20 @@ typedef struct
   Elf64_Xword	st_size;		/* Symbol size */
 } Elf64_Sym;
 
-void readelf(const char* path);
-void readSymtabEntry(FILE* fp, Elf64_Sym* sym);
+#ifdef CONFIG_ISA64
+    typedef Elf64_Ehdr Elf_Ehdr;
+    typedef Elf64_Shdr Elf_Shdr;
+    typedef Elf64_Sym  Elf_Sym;
+    typedef Elf64_Off  Elf_Off;
+    typedef Elf64_Addr Elf_Addr;
+    typedef Elf64_Half Elf_Half;
+#else
+    typedef Elf32_Ehdr Elf_Ehdr;
+    typedef Elf32_Shdr Elf_Shdr;
+    typedef Elf32_Sym  Elf_Sym;
+    typedef Elf32_Off  Elf_Off;
+    typedef Elf32_Addr Elf_Addr;
+    typedef Elf32_Half Elf_Half;
+#endif
+
+void ftrace(word_t pc); 
