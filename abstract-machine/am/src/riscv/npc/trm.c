0f700cbcb6cb6f51222cdbaf78da5aeafcb6816e
waterrr1389 <riyant258316@gmail.com>
2026-03-05 21:41:00 +0800

    实现halt()

--- abstract-machine/am/src/riscv/npc/trm.c
+++ abstract-machine/am/src/riscv/npc/trm.c
@@ -1,5 +1,6 @@
 #include <am.h>
 #include <klib-macros.h>
+#define trap(code) asm volatile("mv a0, %0; ebreak" : :"r"(code))
 
 extern char _heap_start;
 int main(const char *args);
@@ -15,6 +16,9 @@ void putch(char ch) {
 }
 
 void halt(int code) {
+  trap(code);
+
+  // should not reach here
   while (1);
 }
 
